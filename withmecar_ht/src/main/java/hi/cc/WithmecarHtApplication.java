2c6708451a0c49c0aca0ef014d881f917a2ab907
xhz0906 <1522188472@qq.com>
2019-09-11 18:27:56 +0800

    lxz

---
+++ withmecar_ht/src/main/java/hi/cc/WithmecarHtApplication.java
@@ -0,0 +1,13 @@
+package hi.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithmecarHtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithmecarHtApplication.class, args);
+    }
+
+}
