9d7569765d8473aeb12f37e892e8dd905aad4cdb
cy348215 <940955751@qq.com>
2019-09-22 22:57:04 +0800

    cy

---
+++ wirhmecar_pojo/src/main/java/hi/car/pojo/CarParam.java
@@ -0,0 +1,11 @@
+package hi.car.pojo;
+
+import lombok.Data;
+
+@Data
+public class CarParam {
+    private int id;//参数id
+    private String name;//参数名
+    private String type;//参数类型
+    private String state;//参数状态
+}
---
+++ wirhmecar_pojo/src/main/java/hi/car/pojo/CarType.java
@@ -0,0 +1,9 @@
+package hi.car.pojo;
+
+import lombok.Data;
+
+@Data
+public class CarType {
+    private int id;//类型id
+    private String name;//类型名
+}
