5e0d394b40efc9f4ec90d093cb8d122e25afa429
cy348215 <940955751@qq.com>
2019-09-24 08:44:30 +0800

    cy

---
+++ wirhmecar_pojo/src/main/java/hi/car/pojo/CarCountry.java
@@ -0,0 +1,9 @@
+package hi.car.pojo;
+
+import lombok.Data;
+
+@Data
+public class CarCountry {
+    private int id;
+    private String country;
+}
--- wirhmecar_pojo/src/main/java/hi/car/pojo/CarLogo.java
+++ wirhmecar_pojo/src/main/java/hi/car/pojo/CarLogo.java
@@ -7,4 +7,5 @@ public class CarLogo {
     private int id;//logoid
     private String name;//logo名
     private String logo;//logo图
+    private String country;
 }
